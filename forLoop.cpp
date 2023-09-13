b2efe4fad43d566f73ff536c21c82812b8eb8a4c
Andrew Hirt <142833305+WowlSowl@users.noreply.github.com>
2023-09-13 17:54:51 +0000

    updated info

---
+++ forLoop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int counter = {0};
+    int variable = {0};
+    int fin ={10};
+for (counter ; counter < fin ;counter ++)
+    {
+        variable = variable + 1;
+        std::cout << variable;
+    }   
+
+}
