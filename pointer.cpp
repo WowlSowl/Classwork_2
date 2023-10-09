abbadf68b77c1913c94b908e40e8e4d0b7cd6d3d
Andrew Hirt <142833305+WowlSowl@users.noreply.github.com>
2023-10-09 18:20:13 +0000

    more pointers stuff :]

--- pointer.cpp
+++ pointer.cpp
@@ -6,10 +6,12 @@
 int main()
 {
     int a{10};
-    int *ptr;
+    int *ptr{nullptr};
 
     cout << "a variable: " << a << "\t" << &a << endl; // & is the pointer in this case
     cout << "ptr variable: " << ptr << "\t" << &ptr << endl;
 
+    ptr = &a;
 
+    cout << "ptr variable: " << ptr << "\t" << &ptr << "\t" << *ptr << endl;
 }
