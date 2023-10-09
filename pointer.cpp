5a1a939fbc87fc8fd9f22afb050767d9a8a7ccb9
Andrew Hirt <142833305+WowlSowl@users.noreply.github.com>
2023-10-09 18:08:39 +0000

    week 8 ptr variable added

---
+++ pointer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+    using std::cout;
+    using std::cin;
+    using std::endl;
+
+int main()
+{
+    int a{10};
+    int *ptr;
+
+    cout << "a variable: " << a << "\t" << &a << endl; // & is the pointer in this case
+    cout << "ptr variable: " << ptr << "\t" << &ptr << endl;
+
+
+}
